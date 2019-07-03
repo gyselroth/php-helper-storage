d35ca4bbce799a3d740a84f0ed37eec2ff4981fe
kay.stenschke <info@stenschke.com>
2019-07-03 14:32:00 +0200

    simplify+correct psr-4 path

--- src/S3c.php
+++ src/S3c.php
@@ -3,7 +3,7 @@
 /**
  * Copyright (c) 2019 gyselroth™  (http://www.gyselroth.net)
  *
- * @package \gyselroth\HelperS3
+ * @package \gyselroth\HelperStorage
  * @author  gyselroth™  (http://www.gyselroth.com)
  * @link    http://www.gyselroth.com
  * @license Apache-2.0
